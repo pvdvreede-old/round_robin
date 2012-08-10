747d4a2f06ad4741f3b07685e185f6f55e8e45fb
pvdvreede <paul@vdvreede.net>
2012-08-11 09:59:03 +1000

    Initial commit with base plugin.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :round_robin do
+  name 'Round Robin plugin'
+  author 'Paul Van de Vreede'
+  description 'Assignment to users in a group via round robin.'
+  version '0.9.0'
+  url 'https://github.com/pvdvreede/round_robin'
+  author_url 'https://github.com/pvdvreede'
+end
